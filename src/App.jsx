c66f9e29a5da48240abd9e0acdca2481c794eae9
Ian Pollack <ianjpollack@gmail.com>
2024-11-18 23:47:38 -0800

    project bones setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import QuotesApp from "./components/QuotesApp";
+import "./styles/QuotesApp.css";
+
+const App = () => {
+  return (
+    <div>
+      <QuotesApp />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/QuotesApp.jsx
@@ -0,0 +1,28 @@
+const QuotesApp = () => {
+  return (
+    <div className="container">
+      <div className="quotes-app">
+        <h1 className="app-heading">Quote</h1>
+        <i className="bx bxs-heart fav-icon" />
+        <div className="quote">
+          <i className="bx bxs-quote-alt-left left-quote"></i>
+          <p className="quote-text">Ask not what your country can do for you, but what you can do for your country.</p>
+          <p className="quote-author">John F. Kennedy</p>
+          <i className="bx bxs-quote-alt-right right-quote" />
+        </div>
+        <div className="circles">
+          <div className="circle-1"></div>
+          <div className="circle-2"></div>
+          <div className="circle-3"></div>
+          <div className="circle-4"></div>
+        </div>
+        <div className="buttons">
+          <button className="btn btn-new">New Quote</button>
+          <button className="btn btn-fav">Add to Favorites</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuotesApp;
